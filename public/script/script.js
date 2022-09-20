8348361200813566ec2eac53a13019eb3485e9f8
sdewli <saurabhdewli@gmail.com>
2022-09-20 16:20:57 +0530

    files addded

---
+++ public/script/script.js
@@ -0,0 +1,11 @@
+
+
+document.getElementById('dice').addEventListener('click',()=>{
+    window.open('https://sdewli.github.io/Dice-Game/','blank')
+})
+
+
+document.getElementById('match').addEventListener('click',()=>{
+    window.open('https://sdewli.github.io/Dice-Game/','blank')
+})
+
